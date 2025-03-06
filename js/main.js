81fe634b96be354f79bfa7fe463ca409a2f34a45
EvaKolesnikova <evakoless@gmail.com>
2025-03-06 16:34:49 +0700

    8 commit

--- js/main.js
+++ js/main.js
@@ -5,27 +5,36 @@ const Card = {
         column: Number,
         index: Number,
         moveCard: Function,
+        completedAt: String,
+        updateCard: Function,
+        totalCardsInSecondColumn: Number,
     },
-    data() {
-        return {
-            completed: 0,
-            completedAt: null,
-        };
+    computed: {
+        completed() {
+            const completedItems = this.list.filter(item => item.done).length;
+            return Math.floor((completedItems / this.list.length) * 100);
+        },
+        isBlocked() {
+            return this.column === 1 && this.totalCardsInSecondColumn >= 5 && this.completed < 100;
+        }
     },
     methods: {
         checkItem(index) {
+            if (this.isBlocked) return;
             const completedItems = this.list.filter(item => item.done).length;
-            this.completed = Math.floor((completedItems / this.list.length) * 100);
-
-            if (this.completed === 100) {
-                this.completedAt = new Date().toLocaleString();
+            const completed = Math.floor((completedItems / this.list.length) * 100);
+            if (completed === 100 && !this.completedAt) {
+                const completedTime = new Date().toLocaleString();
+                console.log('Задача завершена! Устанавливаем время:', completedTime);
+                this.updateCard(this.index, this.column, { completedAt: completedTime });
             }
 
-            if (this.column === 1 && this.completed > 50) {
+            if (this.column === 1 && completed > 50) {
                 this.moveCard({ column: this.column, index: this.index }, 2);
-            } else if (this.column === 2 && this.completed === 100) {
+            } else if (this.column === 2 && completed === 100) {
                 this.moveCard({ column: this.column, index: this.index }, 3);
             }
+            this.$root.checkBlockFirstColumn();
         },
     },
     template: `
@@ -33,7 +42,7 @@ const Card = {
             <h3>{{ title }}</h3>
             <ul>
                 <li v-for="(item, index) in list" :key="index">
-                  <input type="checkbox" v-model="item.done" @change="checkItem(index)" />
+                  <input type="checkbox" v-model="item.done" @change="checkItem(index)" :disabled="item.done || isBlocked"/>
                   {{ item.text }}
                 </li>
             </ul>
@@ -47,6 +56,8 @@ const Column = {
         columnNumber: Number,
         cards: Array,
         moveCard: Function,
+        updateCard: Function,
+        totalCardsInSecondColumn: Number,
     },
     components: { Card },
     template: `
@@ -58,7 +69,10 @@ const Column = {
                   :list="card.list"
                   :column="columnNumber"
                   :index="index"
+                  :completedAt="card.completedAt"
                   :moveCard="moveCard"
+                  :updateCard="updateCard"
+                  :totalCardsInSecondColumn="totalCardsInSecondColumn"
                 />
             </div>
         </div>
@@ -71,7 +85,8 @@ const app = new Vue({
         return {
             newCard: {
                 title: '',
-                list: ['', '', '', ''],
+                list: ['', '', ''],
+                completedAt: null,
             },
             columns: [
                 { cards: JSON.parse(localStorage.getItem('column1')) || [] },
@@ -82,17 +97,48 @@ const app = new Vue({
         };
     },
     methods: {
+        updateCard(index, column, data) {
+            Vue.set(this.columns[column - 1].cards[index], 'completedAt', data.completedAt);
+            this.saveData();
+        },
         moveCard(cardIndex, columnIndex) {
             const card = this.columns[cardIndex.column - 1].cards.splice(cardIndex.index, 1)[0];
             this.columns[columnIndex - 1].cards.push(card);
             this.saveData();
+            this.checkBlockFirstColumn();
+        },
+        checkBlockFirstColumn() {
+            const secondColumnFull = this.columns[1].cards.length >= 5;
+            const firstColumnHasProgressingCard = this.columns[0].cards.some(card => {
+                const completedItems = card.list.filter(item => item.done).length;
+                return Math.floor((completedItems / card.list.length) * 100) > 50;
+            });
+            if (secondColumnFull && firstColumnHasProgressingCard) {
+                this.blockFirstColumn = true;
+            } else if (this.columns[1].cards.length < 5) {
+                this.blockFirstColumn = false;
+            }
         },
         saveData() {
             localStorage.setItem('column1', JSON.stringify(this.columns[0].cards));
             localStorage.setItem('column2', JSON.stringify(this.columns[1].cards));
             localStorage.setItem('column3', JSON.stringify(this.columns[2].cards));
         },
+        addItem() {
+            if (this.newCard.list.length < 5) {
+                this.newCard.list.push('');
+            }
+        },
+        removeItem(index) {
+            if (this.newCard.list.length > 3) {
+                this.newCard.list.splice(index, 1);
+            }
+        },
         addNewCard() {
+            if (this.blockFirstColumn) {
+                alert('Первый столбец заблокирован, пока во втором не появится свободное место');
+                return;
+            }
             if (this.newCard.title.trim() && this.newCard.list.every(item => item.trim())) {
                 const newCard = {
                     title: this.newCard.title,
@@ -102,7 +148,7 @@ const app = new Vue({
                     this.columns[0].cards.push(newCard);
                     console.log('New card added:', newCard);
                 } else {
-                    alert('Первый столбец может содержать не более 3 карточек!');
+                    alert('В первом храниться не более 3-х карточек!');
                 }
                 this.saveData();
                 this.newCard = { title: '', list: ['', '', '', ''] };
@@ -115,19 +161,21 @@ const app = new Vue({
     template: `
     <div id="app">
         <div>
-            <h2>Создать новую заметку</h2>
+            <h2>Create a new note</h2>
             <form @submit.prevent="addNewCard">
-              <div>
-                <label for="title">Title:</label>
-                <input v-model="newCard.title" id="title" type="text" required />
-              </div>
-              <div>
-                <label for="list">Items (min 3, max 5):</label>
-                <div v-for="(item, index) in newCard.list" :key="index">
-                  <input v-model="newCard.list[index]" type="text" :placeholder="'Item ' + (index + 1)" required />
+                <div>
+                    <label for="title">Title:</label>
+                    <input v-model="newCard.title" id="title" type="text" required />
                 </div>
-              </div>
-              <button type="submit">Add Note</button>
+                <div>
+                    <label>Items (min 3, max 5):</label>
+                    <div v-for="(item, index) in newCard.list" :key="index">
+                        <input v-model="newCard.list[index]" type="text" :placeholder="'Item ' + (index + 1)" required />
+                        <button v-if="newCard.list.length > 3" type="button" @click="removeItem(index)">✖</button>
+                    </div>
+                    <button v-if="newCard.list.length < 5" type="button" @click="addItem">Add item</button>
+                </div>
+                <button type="submit" :disabled="blockFirstColumn">Add Note</button>
             </form>
         </div>
         <div class="columns-container">
@@ -137,6 +185,8 @@ const app = new Vue({
             :columnNumber="index + 1"
             :cards="column.cards"
             :moveCard="moveCard"
+            :updateCard="updateCard"
+            :totalCardsInSecondColumn="columns[1].cards.length"
             />
         </div>
     </div>
